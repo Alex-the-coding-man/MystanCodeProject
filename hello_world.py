f0bf62f6229b3104a0f70f2756855869415d0280
Alex-the-coding-man <ananlin3028@gmail.com>
2025-04-30 00:24:58 +0800

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+"""
+File: hello_world.py
+Name: Alex
+-----------------------------------
+This program prints 'hello, world!'
+on Console and shows the basic 
+Python syntax
+"""
+
+
+def main():
+	print('Hello world', end="")
+	print('hhhhhheeeeeelllllllooooooo', end="")
+
+
+# ----- DO NOT MODIFY CODE BELOW THIS LINE ----- #
+if __name__ == '__main__':
+	main()
